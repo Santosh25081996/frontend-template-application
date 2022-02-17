3f4c87b107139b9bd030465b91f1ea4da0a42df9
Santosh Divate <santosh.kumar.divate@accenture.com>
2022-02-17 18:38:28 +0530

    Modified text content

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -2,7 +2,7 @@ export default function ExamplePage() {
   return (
     <main>
       <div className="container-fluid">
-        <h1>Example Page</h1>
+        <h1>This is a sample template mfe</h1>
         <p>Hello world!</p>
       </div>
     </main>
